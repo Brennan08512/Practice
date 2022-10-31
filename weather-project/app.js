d967ab09b6a11bb2b216046367a85762590ea692
Brennan Donahue <brennan08512@gmail.com>
2022-10-31 11:40:24 -0400

    api get weather data work

--- weather-project/app.js
+++ weather-project/app.js
@@ -6,8 +6,26 @@ app.get("/", function (req, res){
     const url = "https://api.openweathermap.org/data/2.5/weather?q=Columbus&appid=6d5c1d3ffcfa23dfb17678688beef14a&units=imperial"
     https.get(url, function(response){
         console.log(response)
+
+        response.on('data', function(data){
+            const weatherData = JSON.parse(data) //turning the data into a JS object
+            const temp = weatherData.main.temp
+            const weatherDescription = weatherData.weather[0].description
+            const icon = weatherData.weather[0].icon
+            const imageUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`
+            res.write('<h1>The temperature in Columbus is ' + temp + ' degrees F.</h1>')
+            res.write(`<p>The weather is currently ${weatherDescription}`)
+            res.write(`<img src=${imageUrl}>`)
+            res.send()
+            console.log(temp)
+            console.log(weatherDescription)
+            // const object = {
+            //     name: 'Brennan',
+            //     favFood:'Ramen'
+            // }
+            // console.log(JSON.stringify(object)) //turn object into a string
+        })
     })
-    res.send('Sever is up and running.')
 })
 
 app.listen(3000, function(){
