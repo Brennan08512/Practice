32a7b4a1c811bc513c488859f9275fa8945b69e7
Brennan Donahue <brennan08512@gmail.com>
2022-10-31 13:13:29 -0400

    Post api work with weather app

--- weather-project/app.js
+++ weather-project/app.js
@@ -1,11 +1,24 @@
 const express = require("express")
 const https = require('https')
+const bodyParser = require("body-parser")
 const app = express()
 
+app.use(bodyParser.urlencoded({extended: true}))
+
 app.get("/", function (req, res){
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Columbus&appid=6d5c1d3ffcfa23dfb17678688beef14a&units=imperial"
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/', function(req, res){
+    console.log('post recieved')
+   
+    const query = req.body.cityName
+    const apiKey = '6d5c1d3ffcfa23dfb17678688beef14a&'
+    const unit = 'imperial'
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${apiKey}units=${unit}`
+
     https.get(url, function(response){
-        console.log(response)
+        console.log(response.statusCode)
 
         response.on('data', function(data){
             const weatherData = JSON.parse(data) //turning the data into a JS object
@@ -13,7 +26,7 @@ app.get("/", function (req, res){
             const weatherDescription = weatherData.weather[0].description
             const icon = weatherData.weather[0].icon
             const imageUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`
-            res.write('<h1>The temperature in Columbus is ' + temp + ' degrees F.</h1>')
+            res.write(`<h1>The temperature in ${query} is ${temp} degrees F.</h1>`)
             res.write(`<p>The weather is currently ${weatherDescription}`)
             res.write(`<img src=${imageUrl}>`)
             res.send()
@@ -27,6 +40,9 @@ app.get("/", function (req, res){
         })
     })
 })
+
+
+
 
 app.listen(3000, function(){
     console.log('Server running on port 3000')
