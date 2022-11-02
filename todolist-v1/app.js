6a1883ac385cecf96a192a76588f4f8cc0b72b1a
Brennan Donahue <brennan08512@gmail.com>
2022-11-02 15:41:28 -0400

    ejs for loop to add items to list

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -3,43 +3,30 @@ const bodyParser = require("body-parser")
 
 
 const app = express()
+app.use(bodyParser.urlencoded({extended:true}))
 app.set('view engine', 'ejs')
+
+let items = []
 
 app.get("/", function(req, res){
 
-    let today = new Date()
-    let currentDay = today.getDay()
-    let day = ''
+    let options = { 
+        weekday: 'long', 
+        year: 'numeric', 
+        month: 'long', 
+        day: 'numeric',
+     };
+    let today  = new Date();
+    let day = today.toLocaleString("en-US", options)
 
-    switch (currentDay) {
-        case 0:
-            day = 'Sunday'
-            break;
-        case 1:
-            day = 'Monday'
-            break;
-        case 2:
-            day = 'Tuesday'
-            break;
-        case 3:
-            day = 'Wednesday'
-            break;
-        case 4:
-            day = 'Thursday'
-            break;
-        case 5:
-            day = 'Friday'
-            break;
-        case 6:
-            day = 'Saturday'
-            break;
-        default:
-            console.log(`Error: day is equal to: ${day}`)
-            break;
-    }
+    res.render('list', {kindOfDay: day, newListItems: items})
 
-    res.render('list', {kindOfDay: day})
+})
 
+app.post('/', function(req, res){
+    let item = req.body.newItem
+    items.push(item)
+    res.redirect('/')
 })
 
 
