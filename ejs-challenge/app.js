1e9010638ede14f0fff5b6eb2826c8aa4549def1
Brennan Donahue <brennan08512@gmail.com>
2022-11-04 21:07:17 -0400

    Finished blog

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -3,6 +3,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+let _ = require("lodash")
+let posts = []
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,7 +18,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get('/', function(req, res){
-  res.render('home', {homeStartingContent: homeStartingContent})
+  res.render('home', {
+    homeStartingContent: homeStartingContent, 
+    posts: posts,
+  })
 })
 
 app.get('/about', function(req, res){
@@ -32,13 +37,28 @@ app.get('/compose', function(req, res){
 })
 
 app.post('/compose', function(req, res){
-
-  console.log(req.body.postTitle)
+  const post = {
+    postTitle: req.body.postTitle,
+    postBody: req.body.postBody
+  }
+  posts.push(post)
+  res.redirect('/')
 })
 
+app.get('/post/:postName', function(req, res){
+  const requestedTitle = _.lowerCase(req.params.postName)
+  posts.forEach(function(post){
+    const postTitle = _.lowerCase(post.postTitle)
+    const postBody = post.postBody
+    if (postTitle === requestedTitle){
+      res.render('post', {
+        postTitle: post.postTitle,
+        postBody: postBody
+      })
+    }
+  })
 
-
-
+})
 
 
 
